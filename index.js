cb08eada9d5d887846e094fb904ba48e33389e4f
Joe Smith <yasumoto7@gmail.com>
2012-05-28 23:35:21 -0700

    Remove extra module, update to latest express version, switch to parse module format. Signed-off-by: Joe Smith <yasumoto7@gmail.com>

---
+++ index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var acl_update = require('parse-user-acl-update');
+
+console.log(acl_update);
+
+// create an express webserver
+var app = express.createServer(
+  express.logger(),
+  express.static(__dirname + '/public'),
+  express.bodyParser(),
+  express.cookieParser(),
+  // set this to a secret value to encrypt session cookies
+  express.session({ secret: process.env.SESSION_SECRET || 'secret123' })
+);
+
+// listen to the PORT given to us in the environment
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
+app.dynamicHelpers({
+  'host': function(req, res) {
+    return req.headers['host'];
+  },
+  'scheme': function(req, res) {
+    req.headers['x-forwarded-proto'] || 'http'
+  },
+  'url': function(req, res) {
+    return function(path) {
+      return app.dynamicViewHelpers.scheme(req, res) + app.dynamicViewHelpers.url_no_scheme(path);
+    }
+  },
+  'url_no_scheme': function(req, res) {
+    return function(path) {
+      return '://' + app.dynamicViewHelpers.host(req, res) + path;
+    }
+  },
+});
+
+
+function apply_user_acls(request, response) {
+  acl_update.update_account_sharing(request, response);
+}
+
+app.get('/', apply_user_acls);
